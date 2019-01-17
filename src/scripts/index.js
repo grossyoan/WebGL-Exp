f672fcd62403e4f61a68f7d2a99e9c8d628b2c59
grossyoan <gross.yoan@gmail.com>
2019-01-17 17:26:36 +0100

    :tada: Initial WebGL commit !

---
+++ src/scripts/index.js
@@ -0,0 +1,75 @@
+import '../css/style.styl'
+import * as THREE from 'three'
+
+/**
+ * Sizes
+ */
+const sizes = {}
+sizes.width = window.innerWidth
+sizes.height = window.innerHeight
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+})
+
+/**
+ * Cursor
+ */
+const cursor = {}
+cursor.x = 0
+cursor.y = 0
+
+window.addEventListener('mousemove', (_event) =>
+{
+    cursor.x = _event.clientX / sizes.width - 0.5
+    cursor.y = _event.clientY / sizes.height - 0.5
+})
+
+/**
+ * Scene
+ */
+const scene = new THREE.Scene()
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(sizes.width, sizes.height)
+renderer.shadowMap.enabled = true
+document.body.appendChild(renderer.domElement)
+
+/**
+ * Loop
+ */
+const loop = () =>
+{
+    window.requestAnimationFrame(loop)
+
+    // Update camera
+    camera.position.x = cursor.x * 3
+    camera.position.y = - cursor.y * 3
+    camera.lookAt(new THREE.Vector3())
+
+    // Renderer
+    renderer.render(scene, camera)
+}
+loop()
